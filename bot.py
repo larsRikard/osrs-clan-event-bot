579dd42849ce2c8b56b9162a65844187e1f75718
Rekkert <lars.r.raadstoga@gmail.com>
2023-08-06 16:13:27 +0200

    bug ⚱️

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-import datetime
 import os
+import json
 import random
+import re
 
 import discord
 from dotenv import load_dotenv
@@ -12,6 +13,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Define the intents your bot requires
 intents = discord.Intents.default()
 intents.message_content = True
+intents.reactions = True
 
 client = discord.Client(intents=intents)
 
@@ -33,20 +35,55 @@ async def on_message(message):
             return
 
         primary_picks = random.sample(primary_events, 4)
+
+        # Check if "Other" is in primary_picks and replace it
         if "Other" in primary_picks:
             primary_picks.remove("Other")
             primary_picks.extend(random.sample(other_events, 1))
 
-        #response = "Randomly picked primary events:\n" + "\n".join(primary_picks)
-        next_sunday = datetime.date.today() + datetime.timedelta(days=(6 - datetime.date.today().weekday()) % 7)
-        
-        response = "!poll title:Stem på event til " + next_sunday.strftime("%d.%m.%Y") + f" answer1:{primary_picks[0]} answer2:{primary_picks[1]} answer3:{primary_picks[2]} answer4:{primary_picks[3]}"
+        response = "**Randomly picked primary events:**\n\n" + "\n".join(primary_picks)
 
-        await message.channel.send(response)
+        # Send the poll message
+        poll_message = await message.channel.send(response)
+
+        # Add reactions to the poll message
+        emojis = [event[-1] for event in primary_picks]
+        for emoji in emojis:
+            print(emoji)
+            print(len(emoji))
+            await poll_message.add_reaction(emoji)
+        """ await poll_message.add_reaction("⚱️") """
+            
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if not user.bot and reaction.message.content.startswith("Randomly picked primary events:"):
+        # Parse the poll message and extract the events
+        events = reaction.message.content.split('\n')[1:]
+        events = [event.split(':')[-1].strip() for event in events]
+
+        # Get the corresponding emoji
+        emoji_reaction = reaction.emoji
+        print(emoji_reaction)
+
+        # Check if the emoji is present in the events
+        for index, event in enumerate(events):
+            emoji_pattern = r'[^a-zA-Z0-9 ]'
+            event_emoji = re.sub(emoji_pattern, '', event[-1])
+            if emoji_reaction == event_emoji:
+                # Increment the vote count for the event
+                event_votes = int(re.findall(r'\d+', event)[0]) + 1
+                events[index] = event.replace(f":{event_votes - 1}:", f":{event_votes}:")
+
+        # Create the updated poll message
+        updated_message = "Randomly picked primary events:\n" + "\n".join(events)
+
+        # Edit the poll message with updated votes
+        await reaction.message.edit(content=updated_message)
 
 def read_event_list():
     try:
-        with open("eventList.txt", "r") as file:
+        with open("eventList.txt", "r", encoding="utf-8") as file:
             events = file.read().strip().splitlines()
         
         primary_events = []
